e4ca6d276bda31d59e108ff72c196d332f31adca
thomdela2 <51121597+thomdela2@users.noreply.github.com>
2019-05-29 11:47:15 +0200

    Add files via upload

---
+++ assets/scripts/app.js
@@ -0,0 +1,14 @@
+// // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//     document.getElementById("navigatie").style.padding = "30px 10px";
+//     document.getElementById("logo").style.height = "10vh";
+//   } else {
+//     document.getElementById("navigatie").style.padding = "80px 10px";
+//     document.getElementById("logo").style.fontSize = "12vh";
+//   }
+// }
+
+ScrollReveal().reveal('.slagzin');
